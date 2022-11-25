c899ebb992b4df6dc8cd041ec62878d91b47ae40
Jakub007d <56787294+Jakub007d@users.noreply.github.com>
2022-11-25 21:13:39 +0100

    Added Login function

--- wis2/app/routes.py
+++ wis2/app/routes.py
@@ -1,20 +1,41 @@
-from flask import render_template, request, redirect,url_for
+from flask import render_template, request, redirect,url_for,flash
 from app import app
+from app.models import User
+from app.forms import LoginForm
+from flask_bcrypt import Bcrypt
+import psycopg2
 
-@app.route('/login', methods = ['GET','POST'])
 
+def get_db_connection():
+    conn = psycopg2.connect(
+        host="dpg-ce0ann1a6gdsa60lpc20-a.frankfurt-postgres.render.com",
+        database="wis2",
+        user="miguel",
+        password="F8tZ8MKi3KB4gJko95puK6EB4VOPaB9s")
+    return conn
+
+@app.route('/login', methods=['GET', 'POST'])
 def login():
-
-
-    #if request.method == 'POST':
-    #    if request.form['username'] != 'miguel' or request.form['password'] != 'sanchez':
-    error = 'Invalid Credentials. Please try again.'
-    #    else:
-    #        return redirect(url_for('login'))
-
-    return render_template('index.html', error=error)
-
+    form = LoginForm()
+    if form.validate_on_submit():
+        userModel = User()
+        user = userModel.validatePasswordAndSetUser(form.password.data,form.username.data)
+        if user == False:
+            form.username.errors.append("Zle zadané dáta")
+            return render_template('betterLogin.html',form=form)
+        else:
+            return render_template('home.html')
+    else:
+        return render_template('betterLogin.html',form=form)
 
 @app.route('/', methods=['GET','POST'])
 def mainPage():
-    return render_template('homePage.html')
+    bcript = Bcrypt()
+    conn = get_db_connection()
+    cur = conn.cursor()
+    cur.execute('SELECT * FROM users;')
+    users = cur.fetchall()
+    conn.close()
+    conn.close()
+    print('''<h1>{{users}}</h1>''')
+    return render_template('homePage.html',users=users)
