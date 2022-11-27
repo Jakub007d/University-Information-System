9f870e6d9e40e4be3abfc69b11b265bbfea234fb
Jakub007d <56787294+Jakub007d@users.noreply.github.com>
2022-11-27 23:36:10 +0100

    fswfw

--- wis2/app/__init__.py
+++ wis2/app/__init__.py
@@ -6,6 +6,3 @@ app.config.from_object(Config)
 app.permanent_session_lifetime = timedelta(hours=2)
 
 from app import routes
-
-if __name__=="__main__":
-    app.run(debug=True,host="127.0.0.1", port=5000)
